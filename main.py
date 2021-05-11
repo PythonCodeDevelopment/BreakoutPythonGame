033d750e52b7f201691c79c86030a4059c942a8b
kuntal patel <kuntalbahennarshibhaipatel@my.unt.edu>
2021-05-10 20:14:06 -0500

    animate the square and add background color

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 
 pygame.init()
 
+BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
 screen = pygame.display.set_mode((600, 800))
@@ -13,11 +14,11 @@ square = pygame.rect.Rect(0, 0, 50, 50)
 running = True
 
 while running:
-    
+#animate the square
     square.x += 5
     square.y += 5
-    
-    
+    #add background color
+    screen.fill(BLACK)
     pygame.draw.rect(screen, RED, square)
     pygame.display.flip()
 
