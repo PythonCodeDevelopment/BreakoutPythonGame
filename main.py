1c35be457b715b688c0d539b858649797ff54601
kuntal patel <kuntalbahennarshibhaipatel@my.unt.edu>
2021-05-13 17:38:56 -0500

    center ball on the screen

--- main.py
+++ main.py
@@ -10,12 +10,19 @@ RED = (255, 0, 0)
 #variablize these values if need different size output window
 screen = pygame.display.set_mode((600, 800))
 
+screen_rect = screen.get_rect()
+
 #control game speed
 #new clock
 clock = pygame.time.Clock()
 
 
 square = pygame.rect.Rect(0, 0, 50, 50)
+
+square.center = screen_rect.center
+
+x_speed = 5
+y_speed = 5
 
 
 running = True
@@ -25,8 +32,9 @@ while running:
     
     clock.tick(1)
   #1 frame per second
-    square.x += 5
-    square.y += 5
+    
+    square.x += x_speed
+    square.y += y_speed
     #add background color
     screen.fill(BLACK)
     pygame.draw.rect(screen, RED, square)
