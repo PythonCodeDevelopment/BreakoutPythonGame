7f2cf5347df3657ab6e562fe3aff718bc4f1a5a3
kuntal patel <kuntalbahennarshibhaipatel@my.unt.edu>
2021-05-14 08:20:00 -0500

    refactored working code with ball class

--- main.py
+++ main.py
@@ -33,17 +33,17 @@ class Ball(pygame.sprite.Sprite):
 
         #bouncing off the side walls
     
-        if square.right >= screen_rect.right:
+        if self.rect.right >= screen_rect.right:
             self.x_speed = -5
-        if square.left <= screen_rect.left:
+        if self.rect.left <= screen_rect.left:
             self.x_speed = 5
-        if square.top <= screen_rect.top:
+        if self.rect.top <= screen_rect.top:
             self.y_speed = 5
-        if square.bottom >= screen_rect.bottom:
+        if self.rect.bottom >= screen_rect.bottom:
             self.y_speed = -5
     
-        square.x += self.x_speed
-        square.y += self.y_speed
+        self.rect.x += self.x_speed
+        self.rect.y += self.y_speed
 
 #create sprite group
 all_sprites = pygame.sprite.Group()
